e0f09b873ff69b9eb9bed653d633834bb7d7682c
Michael Collins <michaelj.collinsuk@gmail.com>
2016-01-25 15:40:26 +0000

    #Closed 4

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,6 +1,11 @@
+require 'oystercard.rb'
+
 describe Oystercard do
+  subject(:oyster) { described_class.new }
 
-
+  it 'has a default balance of 0' do
+    expect(oyster.balance).to eq 0
+  end
 
 
 end
