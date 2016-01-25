90811458085bbee72121ad2927df4e1592d637d6
Hannah Smyth-Osbourne <h.smythosbourne@gmail.com>
2016-01-25 15:53:34 +0000

    #Closed 5: top_up method

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -7,5 +7,16 @@ describe Oystercard do
     expect(oyster.balance).to eq 0
   end
 
+   describe '#top-up' do
+   		it 'can top up by amount passed in' do
+   			expect{ subject.top_up(1)}.to change{ subject.balance}.by 1   			
+   		end
+   		
+   	end
+
+
+
+
+
 
 end
