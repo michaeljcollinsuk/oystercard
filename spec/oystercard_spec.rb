ca4f2a9b667dcf4c79aba7a8b7d86373b73b03b8
jonathan pickard <Jonathandpickard@gmail.com>
2016-01-26 10:26:14 +0000

    added oystercard class

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,10 @@
-describe Oystercard do
+require 'oystercard'
 
+describe Oystercard do
+  subject(:oystercard) { described_class.new }
+  it 'initializes with a default balance of 0' do
+    expect(oystercard.balance).to eq 0
+  end
+
+  
 end
